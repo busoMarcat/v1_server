0f3728d83e7606dfd58e40446faeb4a94de0ebd7
jyj1289 <jin37091289@gmail.com>
2023-07-24 23:53:37 +0900

    add : test

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
-
 const port = 3000;
+
+const test = require('./routes/testRouter')
+app.use('/user', test);
+
 app.listen(port, () => {
   console.log(`listening  at http://localhost:${port}`);
 });
