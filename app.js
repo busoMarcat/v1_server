ac107f671214398b57d3b2a8ff46df2d89938157
jyj1289 <jin37091289@gmail.com>
2023-07-25 23:29:00 +0900

    refactor : create, read 라우터를 boardRouter로 통합

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const create = require('./routes/createRouter');
-app.use('/board', create);
-
-const read = require('./routes/readRouter');
-app.use('/board', read);
+const boardRouter = require('./routes/boardRouter');
+app.use('/board', boardRouter);
 
 app.listen(port, () => {
   console.log(`listening  at http://localhost:${port}`);
