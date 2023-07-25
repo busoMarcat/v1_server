bb291a5b3a484578e6974c20ffbd7f4ef878400c
jyj1289 <jin37091289@gmail.com>
2023-07-25 23:51:05 +0900

    refactor : require 가독성 개선

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
 const boardRouter = require('./routes/boardRouter');
+
 app.use('/board', boardRouter);
 
 app.listen(port, () => {
