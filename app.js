d83b1526366ce16a1f8e5a100e5cfe0750653aba
jyj1289 <jin37091289@gmail.com>
2023-07-25 23:10:16 +0900

    add : create, read, detail 미들웨어

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const test = require('./routes/testRouter')
+const test = require('./routes/testRouter');
 app.use('/user', test);
+
+const create = require('./routes/createRouter');
+app.use('/board', create);
+
+const read = require('./routes/readRouter');
+app.use('/board', read);
+
+const detail = require('./routes/detailRouter');
+app.use('/board', detail);
 
 app.listen(port, () => {
   console.log(`listening  at http://localhost:${port}`);
 });
+
+
